7c16ec99a8130f4cd0aae71dd3bbb46b8e3dd573
Mathi-MS <sammathi17@gmail.com>
2024-05-15 19:24:31 +0530

    sidebar

--- Assests/script.js
+++ Assests/script.js
@@ -30,3 +30,13 @@ navright.addEventListener("click",function () {
     }
 })
 
+// sidebar 
+var toggle = document.querySelector(".toggle")
+var sidebar = document.querySelector(".sidebar")
+var sideclose = document.querySelector(".sideclose")
+toggle.addEventListener("click",function () {
+    sidebar.style.marginLeft="0px"
+})
+sideclose.addEventListener("click",function () {
+    sidebar.style.marginLeft="-100%"
+})
