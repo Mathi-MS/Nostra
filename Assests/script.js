93148ed9039de7b12c3c9d716faadb18257e1d9e
Mathi-MS <sammathi17@gmail.com>
2024-05-15 18:13:35 +0530

    landing page

---
+++ Assests/script.js
@@ -0,0 +1,32 @@
+//alertclose
+var alertclose = document.querySelector(".alertclose")
+
+alertclose.addEventListener("click",function () {
+    alertclose.closest(".alert").remove()
+})
+//slider
+var navleft = document.querySelector(".nav__left")
+var navright = document.querySelector(".nav__right")
+var scroll = document.querySelector(".slider__images")
+var scrollmargin = 0
+navleft.addEventListener("click",function () {
+    if(scrollmargin == 0){
+        scrollmargin=200
+        scroll.style.marginLeft ="-"+scrollmargin+"vw"
+    }
+    else{
+        scrollmargin = scrollmargin - 100
+        scroll.style.marginLeft ="-"+scrollmargin+"vw"
+    }
+})
+navright.addEventListener("click",function () {
+    scrollmargin = scrollmargin + 100
+    if(scrollmargin > 200){
+        scroll.style.marginLeft = "0vw"
+        scrollmargin = 0
+    }
+    else{
+        scroll.style.marginLeft ="-"+scrollmargin+"vw"
+    }
+})
+
