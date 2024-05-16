edabba0cbf76d990c8ac7efbdc65f8e9158c37dc
Mathi-MS <sammathi17@gmail.com>
2024-05-16 19:37:27 +0530

    collection page added

--- Assests/collections.js
+++ Assests/collections.js
@@ -13,6 +13,7 @@ sideclose.addEventListener("click",function () {
 var searchinput = document.getElementById("searchinput")
 var collectioncontainer = document.querySelector(".collections__rightboxes")
 var collection = collectioncontainer.querySelectorAll(".collections__rightbox")
+var notfound = collectioncontainer.querySelector("h3")
 
 
 searchinput.addEventListener("keyup",function () {
@@ -21,9 +22,11 @@ searchinput.addEventListener("keyup",function () {
         var collectionname = collection[i].querySelector("h2").textContent.toUpperCase()
         if (collectionname.indexOf(searchvalue)<0) {
             collection[i].style.display="none"
+            notfound.style.display="block"
         }
         else{
             collection[i].style.display="block"
+            notfound.style.display="none"
         }
     }
 })
